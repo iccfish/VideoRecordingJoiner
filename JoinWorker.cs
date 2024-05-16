ef8aa01f07892af51855ba36c8c86f18d45e36ae
木魚(iFish) <iccfish@qq.com>
2024-05-16 20:24:51 +0800

    init.

---
+++ JoinWorker.cs
@@ -0,0 +1,289 @@
+﻿using FSLib.Extension;
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace VideoRecordingJoiner
+{
+	internal class JoinWorker
+	{
+		public bool   IsWin              { get; }      = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+		public bool   IsGroupByMonth     { get; set; } = false;
+		public string FileNameFormat     { get; set; } = string.Empty;
+		public bool   DeleteAfterCombine { get; set; } = false;
+		public bool   NeedEncodeAudio    { get; set; } = false;
+		public string Target             { get; set; } = string.Empty;
+
+		/// <summary>
+		/// 获得或设置文件类型，默认为mkv
+		/// </summary>
+		public string FileType { get; set; } = "mkv";
+
+		public string FfmpegCmd() => IsWin ? "ffmpeg.exe" : "ffmpeg";
+
+		DateTime? ParseDateTimeFromFileName(string fileNameWithoutExt)
+		{
+			var m = Regex.Match(fileNameWithoutExt, @"^\d+M\d+S_(\d+)$", RegexOptions.IgnoreCase);
+			if (m.Success)
+			{
+				return DateTimeEx.FromUnixTicks(m.GetGroupValue(1).ToInt64());
+			}
+			//20240320192213_20240320193309.mp4
+			m = Regex.Match(fileNameWithoutExt, @"^(\d{14})_\d{14}$", RegexOptions.IgnoreCase);
+			if (m.Success)
+			{
+				var time = DateTime.ParseExact(m.GetGroupValue(1), "yyyyMMddHHmmss", null);
+				return time;
+			}
+
+			return null;
+		}
+
+		// 获得目标文件名
+		string GetTargetPathName(int key)
+		{
+			var year  = key >> 9;
+			var month = key >> 5 & 0b1111;
+			var day   = key      & 0b11111;
+
+			return FileNameFormat.Replace("yyyy", year.ToString("d4")).Replace("MM", month.ToString("d2")).Replace("dd", day.ToString("d2")) + "." + FileType;
+		}
+
+		bool DeleteFiles(string[] target)
+		{
+			Console.WriteLine("- 正在删除源文件");
+			try
+			{
+				foreach (var file in target)
+				{
+					Console.WriteLine($"  - {file}");
+					File.Delete(file);
+				}
+				Console.WriteLine("- 删除源文件完成！");
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine($"  - 删除失败：{e}");
+				return false;
+			}
+
+			return true;
+		}
+
+		bool RenameFiles(string[] target)
+		{
+			Console.WriteLine("- 正在重命名源文件");
+			try
+			{
+				foreach (var file in target)
+				{
+					Console.WriteLine($"  - {file}");
+					File.Move(file, file + ".old");
+				}
+				Console.WriteLine("- 重命名源文件完成！");
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine($"  - 重命名失败：{e}");
+				return false;
+			}
+
+			return true;
+		}
+
+		async Task<bool> CombineAsync(string[] src, string outFile, bool encodeAudio)
+		{
+			var tmpFile = Path.GetTempFileName();
+			File.WriteAllLines(tmpFile, src.Select(s => $"file '{Path.GetFullPath(s)}'").ToArray());
+
+			var cmdLine = $"-safe 0 -f concat -i \"{tmpFile}\" -c:v copy {(encodeAudio && NeedEncodeAudio ? "-c:a aac" : "-c:a copy")} -f {FileType} \"{outFile}\"";
+			var psi = new ProcessStartInfo(FfmpegCmd(), cmdLine)
+			{
+				RedirectStandardError  = true,
+				RedirectStandardOutput = true,
+				UseShellExecute        = false,
+				StandardOutputEncoding = Encoding.UTF8,
+				StandardErrorEncoding  = Encoding.UTF8,
+				CreateNoWindow         = true,
+				WindowStyle            = ProcessWindowStyle.Hidden
+			};
+			var codecNotSupport = false;
+
+			Task HandleStreamAsync(StreamReader sr)
+			{
+				string buffer;
+				while (!(buffer = sr.ReadLine()!).IsNullOrEmpty())
+				{
+					Console.WriteLine(buffer);
+
+					if (buffer.Contains("codec not currently supported in container"))
+					{
+						codecNotSupport = true;
+					}
+				}
+				return Task.CompletedTask;
+			}
+
+			var p = Process.Start(psi)!;
+			await Task.WhenAll(
+					Task.Factory.StartNew(() => HandleStreamAsync(p.StandardOutput)),
+					Task.Factory.StartNew(() => HandleStreamAsync(p.StandardError)),
+					Task.Factory.StartNew(() => p.WaitForExit())
+				)
+				.ConfigureAwait(false);
+			File.Delete(tmpFile);
+
+			if (p.ExitCode == 0 && new FileInfo(outFile).Length > 0) return true;
+
+			if (File.Exists(outFile))
+				File.Delete(outFile);
+			if (codecNotSupport && !NeedEncodeAudio)
+			{
+				Console.WriteLine("警告：检测到编码错误，尝试强制转码音频！");
+				NeedEncodeAudio = true;
+				return await CombineAsync(src, outFile, encodeAudio).ConfigureAwait(false);
+			}
+			return false;
+		}
+
+
+		public List<string> Srcs { get; } = new List<string>();
+
+		public async Task JoinAsync()
+		{
+			if (FileNameFormat.IsNullOrEmpty())
+				FileNameFormat = IsGroupByMonth ? "yyyy-MM" : $"yyyy-MM{Path.DirectorySeparatorChar}dd";
+
+			// 定位所有文件和目录
+			Console.WriteLine("正在搜索文件和目录...");
+
+			var files = Srcs.SelectMany(
+					s => Directory.Exists(s) ? Directory.GetFiles(s, "*.mp4", SearchOption.AllDirectories) : File.Exists(s) ? new[] { s } : new string[0] { })
+				.ToArray();
+			Console.WriteLine($"搜索到了 {files.Length} 个文件，正在预处理 ...");
+
+			var tasks = new List<(DateTime dateTime, string path)>();
+
+			foreach (var file in files)
+			{
+				var m = ParseDateTimeFromFileName(Path.GetFileNameWithoutExtension(file));
+				if (m != null)
+					tasks.Add((m.Value, file));
+			}
+
+			if (tasks.Count == 0)
+			{
+				Console.WriteLine("请通过参数指定要合并的文件或文件夹！");
+				return;
+			}
+			if (Target.IsNullOrEmpty())
+			{
+				Target = Environment.CurrentDirectory;
+				Console.WriteLine("未指定输出目录，自动设定为当前目录");
+			}
+			Console.WriteLine($"合并后的文件位置在：{Target}");
+			if (DeleteAfterCombine) Console.WriteLine("合并后将会删除源文件");
+			else Console.WriteLine("合并后将会重命名源文件");
+			Console.WriteLine($"将要合并 {tasks.Count} 个视频");
+
+			// 对任务进行分组
+			var tgs = tasks.GroupBy(s => (s.dateTime.Year << 9) | (s.dateTime.Month << 5) | (IsGroupByMonth ? 0 : s.dateTime.Day)).Select(s => new { key = s.Key, list = s.OrderBy(x => x.dateTime).Select(x => x.path).ToArray() }).OrderBy(s => s.key).ToList();
+			foreach (var tg in tgs)
+			{
+				var outName = GetTargetPathName(tg.key);
+				var outFile = Path.Combine(Target, outName);
+				var tmpFile = outFile + ".tmp";
+
+
+				Console.WriteLine($"=> 初次合并，目标路径：{outFile}");
+				Console.WriteLine($"=> 包含 {tg.list.Length} 视频：");
+				for (int i = 0; i < tg.list.Length; i++)
+				{
+					Console.WriteLine($"  [{i + 1:00000}] {tg.list[i]}");
+				}
+				Console.WriteLine();
+
+				try
+				{
+					Directory.CreateDirectory(Path.GetDirectoryName(outFile)!);
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine($"{tg.key} => 创建文件夹失败：{e.Message}");
+					continue;
+				}
+
+				if (await CombineAsync(tg.list, tmpFile, true).ConfigureAwait(false))
+				{
+					Console.WriteLine("- 合并成功！");
+
+					if (File.Exists(outFile))
+					{
+						Console.WriteLine("- 已存在原始合并文件，二次合并！");
+						if (await CombineAsync(new[] { outFile, tmpFile }, tmpFile + ".swp", false).ConfigureAwait(false))
+						{
+							Console.WriteLine("- 二次合并完成！");
+							File.Delete(outFile);
+							File.Delete(tmpFile);
+							File.Move(tmpFile + ".swp", outFile);
+						}
+						else
+						{
+							Console.WriteLine("- 二次合并失败！");
+							continue;
+						}
+					}
+					else
+					{
+						File.Move(tmpFile, outFile);
+					}
+
+					if (DeleteAfterCombine)
+						DeleteFiles(tg.list);
+					else RenameFiles(tg.list);
+				}
+				else
+				{
+					Console.WriteLine("- 合并失败！");
+					if (File.Exists(tmpFile))
+						File.Delete(tmpFile);
+				}
+			}
+		}
+
+		/// <summary>
+		/// 校验FFMPEG是否安装
+		/// </summary>
+		/// <returns></returns>
+		public async Task<bool> CheckFfmpegAsync()
+		{
+			var psi = new ProcessStartInfo(FfmpegCmd(), "-version")
+			{
+				WindowStyle            = ProcessWindowStyle.Hidden,
+				RedirectStandardError  = true,
+				RedirectStandardOutput = true,
+				CreateNoWindow         = true,
+				UseShellExecute        = false
+			};
+			try
+			{
+				var p = Process.Start(psi)!;
+				await p.StandardOutput.ReadToEndAsync().ConfigureAwait(false);
+				await p.StandardError.ReadToEndAsync().ConfigureAwait(false);
+				await p.WaitForExitAsync().ConfigureAwait(false);
+
+				return p.ExitCode == 0;
+			}
+			catch (Exception)
+			{
+				return false;
+			}
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using FSLib.Extension;
+
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+
+using VideoRecordingJoiner;
+
+Console.WriteLine(
+	$@"监控视频合并工具 by 木魚
+============================
+可用选项：
+  -o	<路径>	指定输出目录; 如果不指定，将默认输出当前运行目录下
+  -d		指定合并后删除源文件，如果不指定此选项，则会修改源文件名（防止重复合并）
+  -f	<格式>	输出文件名格式，不包含扩展名，支持 yyyy、MM、dd占位符，如 ""yyyy-MM{Path.DirectorySeparatorChar}dd""
+  -t	<类型>	输出文件类型，可选 mkv 或 mp4，默认为 mkv，建议使用 mkv
+  -gm		按月合并（默认为按日合并）
+
+注意：
+  - 按月合并模式下，默认输出文件名模板为 【yyyy-MM】，可以使用 -f 选项覆盖
+  - 按日合并模式下，默认输出文件名模板为 【yyyy-MM{Path.DirectorySeparatorChar}dd】，可以使用 -f 选项覆盖
+");
+
+var worker = new JoinWorker();
+for (var i = 0; i < args.Length; i++)
+{
+	switch (args[i])
+	{
+		case "-o":
+			worker.Target = args[++i];
+			break;
+		case "-d":
+			worker.DeleteAfterCombine = true;
+			break;
+		case "-gm":
+			worker.IsGroupByMonth = true;
+			break;
+		case "-f":
+			worker.FileNameFormat = args[++i];
+			break;
+		case "-t":
+			var type = args[++i];
+			if (type is "mkv" or "mp4")
+				worker.FileType = type;
+			else
+			{
+				Console.WriteLine("错误： -t 指定的格式需要为 mp4 或 mkv 其中之一。");
+				return;
+			}
+			break;
+		default:
+			worker.Srcs.Add(args[i]);
+			break;
+	}
+}
+
+if (!await worker.CheckFfmpegAsync().ConfigureAwait(false))
+{
+	Console.WriteLine($"错误：未检测到 ffempg。本程序依赖ffmpeg。请确保相关软件包已安装，或当前目录下存在 {worker.FfmpegCmd()} 且可正常执行。");
+	return;
+}
+await worker.JoinAsync().ConfigureAwait(false);
