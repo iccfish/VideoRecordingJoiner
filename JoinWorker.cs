7237743c316ddc0c051a16588be3177577dee2a0
木魚(iFish) <iccfish@qq.com>
2024-07-18 02:53:58 +0800

    取消延迟中的token

--- JoinWorker.cs
+++ JoinWorker.cs
@@ -237,7 +237,7 @@ namespace VideoRecordingJoiner
 							break;
 					}
 				}
-				else await Task.Delay(50, cancellationToken).ConfigureAwait(false);
+				else await Task.Delay(50).ConfigureAwait(false);
 			}
 			Console.WriteLine();
 		}
